de402bc786b94c7a8d175a02f030b165447db403
Maulana209109 <maulanaktg3@gmail.com>
2023-11-26 22:54:48 +0700

    Modul 2

---
+++ TPModul2_M/Bus.java
@@ -0,0 +1,14 @@
+
+public class Bus extends Kendaraan {
+    private int jmlkursi;
+
+    public Bus(String noidentifikasi, double kecepatanmax, int kapPenumpang, int jmlkursi) {
+        super(noidentifikasi, kecepatanmax, kapPenumpang);
+        this.jmlkursi = jmlkursi;
+    }
+
+    public String toString() {
+        return super.toString() +
+                "Jumlah Kursi: " + jmlkursi;
+    }
+}
